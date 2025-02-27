a224710d0df6d9347bb42e62d7d880adad7b2479
ht丶 <HuoTianBss@163.com>
2025-02-27 09:16:27 +0800

    2025/02/27

--- 大一下/src/实训2/平年闰年.java
+++ 大一下/src/实训2/平年闰年.java
@@ -5,7 +5,7 @@ public class 平年闰年 {
         Scanner year=new Scanner(System.in);
         System.out.print("请输入需要判断的年份：");
         int Year=year.nextInt();
-        if((Year%4==0&&Year%100!=0)|Year%400==0){
+        if(Year%4==0&&Year%100!=0||Year%400==0){
             System.out.println(Year+"年是闰年");
         } else {
                 System.out.println(Year+"年是平年");
