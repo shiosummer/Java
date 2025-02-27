72efb8e871eca59d045f8094bd6f9ce9b1866e83
ht丶 <HuoTianBss@163.com>
2025-02-27 09:02:40 +0800

    2025/02/27

--- 大一下/src/实训2/判断季节.java
+++ 大一下/src/实训2/判断季节.java
@@ -4,12 +4,17 @@ import java.util.Scanner;
 public class 判断季节 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.print("请输入需要判断的月份:");
         int month=sc.nextInt();
-        if(month==3|month==4|month==5){System.out.println(month+"月是春季");}
-        else if(month==6|month==7|month==8){System.out.println(month+"月是夏季");}
-        else if(month==9|month==10|month==11){System.out.println(month+"月是秋季");}
-        else if(month==12|month==1|month==2){System.out.println(month+"月是冬季");}
-        else {System.out.println("输入的月份错误");}
+        if(month==3|month==4|month==5){
+            System.out.println(month+"月是春季");
+        } else if(month==6|month==7|month==8){
+            System.out.println(month+"月是夏季");
+        } else if(month==9|month==10|month==11){
+            System.out.println(month+"月是秋季");
+        } else if(month==12|month==1|month==2){
+            System.out.println(month+"月是冬季");
+        } else {System.out.println("输入的月份错误");}
         sc.close();
         }
     }
---
+++ 大一下/src/实训2/基本输入和输出_1.java
@@ -0,0 +1,16 @@
+package 实训2;
+import java.util.Scanner;
+
+public class 基本输入和输出_1 {
+    public static void main(String[] args) {
+        Scanner a=new Scanner(System.in);
+        System.out.print("请输入第三个整数:");
+        String num=a.nextLine();
+        String[] nums=num.split(" ");
+        int A=Integer.parseInt(nums[0]);
+        int B=Integer.parseInt(nums[1]);
+        int C=Integer.parseInt(nums[2]);
+        System.out.println(nums[0]+"+"+nums[1]+"+"+nums[2]+"="+(A+B+C));
+        a.close();
+    }
+}
