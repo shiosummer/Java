85c83d6cff3c0b1979a21709ffe31938ae9cd92f
ht丶 <HuoTianBss@163.com>
2025-03-03 09:33:21 +0800

    2025/03/03

---
+++ 大一下/src/实训3/体重分析.java
@@ -0,0 +1,22 @@
+package 实训3;
+
+import java.util.Scanner;
+
+public class 体重分析 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("请输入身高(cm):");
+        double Height = sc.nextInt();
+        System.out.print("请输入体重(kg):");
+        double Weight = sc.nextInt();
+        double Standard;
+        Standard = (Height - 100) * 0.9;
+        if (Standard - Weight > 5) {
+            System.out.println("体重不足，加强营养");
+        } else if (Weight - Standard > 5) {
+            System.out.println("体重超重，加强运动");
+        } else {
+            System.out.println("体重正常，注意保持");
+        }
+    }
+}
---
+++ 大一下/src/实训3/判断月份季节.java
@@ -0,0 +1,35 @@
+package 实训3;
+
+import java.util.Scanner;
+
+public class 判断月份季节 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("请输入月份：");
+        int month = sc.nextInt();
+        switch (month) {
+            case 3:
+            case 4:
+            case 5:
+                System.out.println(month + "月是春季");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println(month + "月是夏季");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println(month + "月是秋季");
+                break;
+            case 12:
+            case 1:
+            case 2:
+                System.out.println(month + "月是冬季");
+                break;
+            default:
+                System.out.println("输入值不是一个正确的月份");
+        }
+    }
+}
