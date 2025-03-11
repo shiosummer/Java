d4b57409f00f715f0ece7569c57d42aad2acce83
ht丶 <HuoTianBss@163.com>
2025-03-11 23:25:22 +0800

    2025/03/11

--- 大一下/src/实训1/自我介绍.java
+++ 大一下/src/实训1/自我介绍.java
@@ -1,6 +1,7 @@
 package 实训1;
+
 public class 自我介绍 {
-public static void main(String[] args) {
-	System.out.println("我是郑蔚楠，我的学号是24070110155");
-}
+    public static void main(String[] args) {
+        System.out.println("我是郑蔚楠，我的学号是24070110155");
+    }
 }
---
+++ 大一下/src/实训4/金字塔2.java
@@ -0,0 +1,14 @@
+package 实训4;
+
+class 金字塔2 {
+    public static void main(String[] args) {
+        for (int i=1;i<=5;i++){
+            for (int j=5;j>i;j--){
+                System.out.print("0");
+            }
+            for (int k=10;k>=0;k--){
+            }
+            System.out.println();
+        }
+    }
+}
