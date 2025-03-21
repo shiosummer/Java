505b53c9435249211b89d8285248abb52b0f1c9b
ht丶 <HuoTianBss@163.com>
2025-03-21 11:21:25 +0800

    2025/03/21

---
+++ 大一下/src/实训6/选择排序.java
@@ -0,0 +1,7 @@
+package 实训6;
+
+public class 选择排序 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 大一下/src/实训6/遍历与累加和.java
@@ -0,0 +1,30 @@
+package 实训6;
+
+import java.util.Scanner;
+
+public class 遍历与累加和 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] array=new double[10];
+        for (int i=0;i<array.length;i++){
+            System.out.print("请输入第"+(i+1)+"个小数：");
+            array[i]=sc.nextDouble();
+        }
+        sc.close();
+        printArr(array);
+        System.out.println("数组中所有元素的累加和为"+getSum(array));
+    }
+    public static void printArr(double[] array){
+        for (int i=0;i<array.length;i++){
+            System.out.print(array[i]+" ");
+        }
+        System.out.println();
+    }
+    public static double getSum(double[] array){
+        double sum=0;
+        for (int i=0;i<array.length;i++){
+            sum+=array[i];
+        }
+        return sum;
+    }
+}
