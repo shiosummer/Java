687d9f43f89b08948ef857d2675b7101d807b021
Donk <HuoTianBss@163.com>
2025-04-10 15:55:15 +0800

    2025/04/09

---
+++ 大一下/src/实训7/BooksInfo/BookInfo_test.java
@@ -0,0 +1,24 @@
+package 实训7.BooksInfo;
+
+import java.awt.print.Book;
+
+public class BookInfo_test {
+    public static void main(String[] args) {
+        //构建第一个对象
+        BookInfo Book_1=new BookInfo();
+        Book_1.setBookName("Python程序设计");
+        Book_1.setBookID("9787302550839");
+        Book_1.setPublisher("清华大学出版社");
+        Book_1.setPrice(59.80);
+
+        //构建第二个对象
+        BookInfo Book_2=new BookInfo("Java程序设计","9787302677482","清华大学出版社",69);
+
+        System.out.println("书籍1的信息如下：");
+        Book_1.ShowInfo();
+        System.out.println();
+
+        System.out.println("书籍2的信息如下：");
+        Book_2.ShowInfo();
+    }
+}
